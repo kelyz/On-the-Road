34427d5052ec6105bb400a480d02d140d730ceeb
Kelly Zhang <Kelly@nbp-213-18.nbp.ruw.rutgers.edu>
2017-10-17 01:42:15 -0400

    gunicorn secret key fix

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import app
 
-if __name__ == "__main__":
-	app.secret_key = os.environ['app.secret_key']
+app.secret_key = os.environ['app.secret_key']
+
+	#if __name__ == "__main__":
 	# app.run(host='0.0.0.0', port=5000)
