65ade748c94886cd3bd8ae81917be391fd18485f
Kelly Zhang <Kelly@nbp-250-119.nbp.ruw.rutgers.edu>
2017-10-20 01:07:13 -0400

    Added comments

--- app/views.py
+++ app/views.py
@@ -20,35 +20,44 @@ def index():
 @app.route('/search', methods=['GET','POST'])
 def search():
 	if request.method == 'POST':
-
+		#Receive all of the latitude and longitude pairs in between an origin and destination
 		data = request.json
+		#Divide the distance from origin to destination into 10 parts 
+		#to spread out the different venues avaiable on the map
+		#and store the latitude, longitude pairs in a dictionary
 		step_unit = int(len(data) // 10)
 		step_unit = step_unit if not step_unit % 2 else step_unit - 1
 		data_dict = {data[i]: data[i+1] for i in range(0, len(data), step_unit)}
 
+		#Create a query cookie
 		query = session['query']
 
+		#Use utility functions to find available venues at the latitude, longitude pairs
 		fs_places = [util.fs_search(lat, lng) for lat,lng in data_dict.items() 
 					if util.fs_search(lat, lng) != None]
 
 		yelp_places = [util.yelp_search(lat, lng, query) for lat,lng in data_dict.items() 
 					  if util.yelp_search(lat, lng, query) != None]
 
+		#Remove duplicate venues
 		places = list(set(fs_places + yelp_places))
 
 		return jsonify(places)
 
 	else:
+		#Get origin from index page
 		start = request.args.get('origin')
 		origin = gmaps_client.geocode(start)
 		origin_lat = origin[0]['geometry']['location']['lat']
 		origin_lng = origin[0]['geometry']['location']['lng']
 
+		#Get destination from index page
 		end = request.args.get('destination')
 		destination = gmaps_client.geocode(end)
 		dest_lat = destination[0]['geometry']['location']['lat']
 		dest_lng = destination[0]['geometry']['location']['lng']
 
+		#Get query from index page if available
 		query = request.args.get('query')
 		session['query'] = query
 		
